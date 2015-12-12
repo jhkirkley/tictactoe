02c0b4696905d2fb504b2e9bf4c25ea5e7443f22
John Kirkley <john@johnkirkley.com>
2015-12-12 14:32:37 -0500

    set board

--- js/toe.js
+++ js/toe.js
@@ -1,20 +1,57 @@
-var space = [null, null, null, null, null, null, null, null, null];
-var player = "null";
-var status = 0;
+var bplane = [null, null, null, null, null, null, null, null, null];
+var player1Name = "";
+var player2Name = "";
+var player = "";
+var turn = "";
 //status = parseInt(status);
 $(document).ready(function () {
 
-$("#play").click(function(){
-    console.log("Clicked Play");
-    var status = parseInt(Math.random()*100 + 1);
+function boardMsg(x){
+    return $("#board").text(x);
+}
+
+function init() {
+  turn = "";
+  boardMsg("");
+  for(var i = 0; i < bplane.length; i++) {
+    bplane[i] = "";
+    $(".space").eq(i).html("<p>" + bplane[i] + "</p>");
+  }
+};
+
+
+function setTurn(){
+    var status = parseInt(Math.floor(Math.random()*100) + 1);
 
     if (status > 50) {
       player = "host";
       $( "#player-one" ).addClass( "active-player" );
+      boardMsg(player1Name+"'s turn now!");
     }
     else {player = "guest";
           $( "#player-two" ).addClass( "active-player" );
+          boardMsg(player1Name+"'s turn now!");
         }
+}
+
+
+
+$("#play").click(function(){
+
+    init();
+
+    player1Name = $("#player-1-inp").val();
+    player2Name = $("#player-2-inp").val();
+
+    if(player1Name=="" || player2Name==""){
+        boardMsg("Please enter all of the player names");
+
+    }
+    else { setTurn()}
+});
+
+$(".space").click(function() {
+      $(this).html("<p>Something</P>");
 });
 
 
