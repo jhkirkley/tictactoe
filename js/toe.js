8f61ceb181c07e81b40ed256ddf932f702206871
John Kirkley <john@johnkirkley.com>
2015-12-12 07:11:53 -0500

    single container

---
+++ js/toe.js
@@ -0,0 +1,24 @@
+var space = [null, null, null, null, null, null, null, null, null];
+var player = "host";
+/*$(document).ready(function () {
+
+        $("#play").click(
+            function () {
+                AlertSave();
+            }
+        );
+    });
+
+function AlertSave() {
+   alert("Alert Message OnClick");
+}
+*/
+$(document).ready(function () {
+
+$("#play").click(function(){
+    alert("The paragraph was clicked.");
+});
+
+
+
+});
