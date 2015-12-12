6b9243c2a68e68a3bfbc5194bbbc2dd70f2b26c4
John Kirkley <john@johnkirkley.com>
2015-12-12 11:03:08 -0500

    added player initialization

--- js/toe.js
+++ js/toe.js
@@ -1,22 +1,20 @@
 var space = [null, null, null, null, null, null, null, null, null];
-var player = "host";
-/*$(document).ready(function () {
-
-        $("#play").click(
-            function () {
-                AlertSave();
-            }
-        );
-    });
-
-function AlertSave() {
-   alert("Alert Message OnClick");
-}
-*/
+var player = "null";
+var status = 0;
+//status = parseInt(status);
 $(document).ready(function () {
 
 $("#play").click(function(){
-    alert("The paragraph was clicked.");
+    console.log("Clicked Play");
+    var status = parseInt(Math.random()*100 + 1);
+
+    if (status > 50) {
+      player = "host";
+      $( "#player-one" ).addClass( "active-player" );
+    }
+    else {player = "guest";
+          $( "#player-two" ).addClass( "active-player" );
+        }
 });
 
 
