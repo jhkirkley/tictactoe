7499c99e5b2aee75f1fedd3b13b6490a5bc22511
John Kirkley <john@johnkirkley.com>
2015-12-23 11:15:35 -0500

    style fixes

--- gruntfile.js
+++ gruntfile.js
@@ -1,40 +1,41 @@
-module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-jscs');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
-    grunt.initConfig({
-        jshint: {
-            all: ['js/**/*js']
-        },
-        watch: {
-        files: ['index.html', 'js/**/*js'],
-        tasks: [],
+module.exports = function (grunt) {
+  grunt.initConfig({
+    jshint: {
+      sample: {
+        src: ['js/*.js']
+      }
+    },
+    jasmine: {
+        src: ['js*.js'],
         options: {
-        livereload: {
-        host: 'localhost',
-        port: 9000
-        // you can pass in any other options you'd like to the https server, as listed here: http://nodejs.org/api/tls.html#tls_tls_createserver_options_secureconnectionlistener
-              },
-        debounceDelay: 250,
-
-                  },
-         jasmine: {
-             src: './js/toe.js',
-             options: {
-                 specs: './tests/tests.js'
-             }
-         }
-
-
-        },
-        jscs: {
-        all: [
-            'Gruntfile.js', 'js/**/*js'
-        ],
-        options: {
-            config: '.jscsrc'
+            specs: ['tests/tests.js'],
+            vendor: ['lodash.min.js'],
+            styles: 'css/style.css',
+            template: ['tests/runner.tmpl']
         }
     },
-    });
+
+    connect: {
+      dev: {
+        options: {
+          base: './',
+          port: 3000,
+          livereload: true
+        }
+      }
+    },
+    watch: {
+      www: {
+        files: ['index.html', 'js/*.js', 'css/style.css'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.registerTask('default', []);
 };
